5c4f4d4424960bd126fb11fcb6db4b43f7fa2fd1
mehedi1256 <mehedihassanshovospirit@gmail.com>
2023-10-25 18:02:27 +0600

    Updated

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,42 @@
 */
 
 
+/**
+ * Custom Helpers in Handlebar js
+ * @param(helperName, callback_function)
+ */
+Handlebars.registerHelper("formatName", function(prop1, prop2) {
+    return new Handlebars.SafeString("Hello my name is:<strong>" + prop1 + "</strong> my house name is: <strong>" + prop2 + "</strong>");
+});
+/**
+ * custom helpers for get phone number
+ */
+Handlebars.registerHelper("formatPhoneNumber", function(property) {
+    if(property) {
+        var phone = property.toString();
+        return "(+088)" + 0 + phone.substr(0, 2) + "-" + phone.substr(2);
+    }
+});
+
+
+/**
+ * Block Helper
+ * @param(helper-name, callbackfunc)
+ */
+
+Handlebars.registerHelper("makeBold", function(options) {
+    return new Handlebars.SafeString("<strong>" + options.fn(this) + "</strong>");
+});
+
+
+/**
+ * Block helper
+ * @param(helper-name, callbackfunc)
+ */
+
+Handlebars.registerHelper("toLower", function(options) {
+    return options.fn(this).toLowerCase();
+});
 
 $(document).ready(function() {
     var characterTemplate = $("#character-template").html();
